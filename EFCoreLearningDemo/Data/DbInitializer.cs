0828a97670cbc7adbe794efbd2ce256ddb6d28cc
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-05-08 18:56:29 +0530

    Add project files.

---
+++ EFCoreLearningDemo/Data/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreLearningDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreLearningDemo.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(LearningDbContext context)
+        {
+            if (context.Depts.Any())
+            {
+                return;
+            }
+            var depts = new Dept[]
+            {
+                new Dept{Id=101, Name="Accounting", Loc="Dallas"},
+                new Dept{Id=102, Name="Marketting", Loc="Chicago"},
+                new Dept{Id=103, Name="Sales", Loc="Boston"}
+            };
+            context.Depts.AddRange(depts);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ EFCoreLearningDemo/Data/LearningDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EFCoreLearningDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreLearningDemo.Data
+{
+    public class LearningDbContext:DbContext
+    {
+        public LearningDbContext(DbContextOptions<LearningDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Dept> Depts{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dept>().ToTable("Dept");
+        }
+    }
+}
